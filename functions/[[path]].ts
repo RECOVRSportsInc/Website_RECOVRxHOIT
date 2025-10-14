3b359782fc5347eabc0b8c06a0ed217ef97875a3
RECOVRSportsInc <recovrsports@proton.me>
2025-10-14 16:33:00 -0400

    Update [[path]].ts

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,12 +1,11 @@
-export const onRequest: PagesFunction = async ({ request, next }) => {
-  // Let static files and normal routes resolve first
+// functions/[[path]].ts
+export const onRequest = async ({ request, next }) => {
   const res = await next();
 
   const url = new URL(request.url);
   const isGet = request.method === "GET";
-  const looksLikeAFile = /\.[a-z0-9]+$/i.test(url.pathname); // has .ext
+  const looksLikeAFile = /\.[a-z0-9]+$/i.test(url.pathname);
 
-  // If a GET to a route returns 404 and it is not a file, serve index.html
   if (res.status === 404 && isGet && !looksLikeAFile) {
     const indexResp = await fetch(new URL("/index.html", url));
     return new Response(await indexResp.text(), {
